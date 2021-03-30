2920c1cc9c72e1d6a425c5212b8dbc0c9e1f79d3
Emilia Andersson <emilia.andersson@helsinki.fi>
2021-03-30 20:12:46 +0300

    tehtävä 2

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,27 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+    
+    def test_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
+    
+    def test_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(1000)
+        self.assertEqual(str(self.maksukortti), "saldo: 10.1")
+
+    def test_saldo_vahenee_oikein(self):
+        self.maksukortti.lataa_rahaa(990)
+        self.maksukortti.ota_rahaa(800)
+        self.assertEqual(str(self.maksukortti), "saldo: 2.0")
+    
+    def test_nostaessa_palautuu_true(self):
+        self.maksukortti.lataa_rahaa(990)
+        self.assertEqual(self.maksukortti.ota_rahaa(800), True)
+    
+    def test_liikaa_nostaessa_palautuu_false(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(800), False)
+    
+    def test_saldo_ei_muutu_jos_ei_tarpeeksi_rahaa(self):
+        self.maksukortti.lataa_rahaa(190)
+        self.maksukortti.ota_rahaa(800)
+        self.assertEqual(str(self.maksukortti), "saldo: 2.0")
